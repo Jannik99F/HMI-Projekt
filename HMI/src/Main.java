7ee1399dafd129764fb290feeb194443d60a7937
Mendax1 <abaris004@gmail.com>
2023-01-21 15:29:39 +0100

    Nur Tests in Main zur überprüfung

--- HMI/src/Main.java
+++ HMI/src/Main.java
@@ -1,14 +1,12 @@
-import java.io.IOException;
-import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         //ClickPage clickPage = new ClickPage();
-
+        StartPage startPage = new StartPage();
 
         // Test Methode
-
+/*
         FileHandler fileHandler = new FileHandler();
         ArrayList<Player> players = new ArrayList<>();
         players.add(new Player(100, "Max"));
@@ -16,13 +14,13 @@ public class Main {
         players.add(new Player(300, "Maximilian"));
         players.add(new Player(400, "Elias"));
         players.add(new Player(500, "Johannes"));
-        fileHandler.sort(players);
-        fileHandler.write(players, "HMI/src/scores.txt");
+
+        fileHandler.write(players, 600,"Test","HMI/src/scores.txt");
         ArrayList<Player> stuff = fileHandler.readFile("HMI/src/scores.txt");
         for (Player player : stuff) {
             System.out.println(player.getScore() + " " + player.getName());
         }
-
+*/
 
 
     }

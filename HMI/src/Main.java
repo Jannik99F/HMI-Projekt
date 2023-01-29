ac887eefc4bfdc481649c8fe376961b852937c00
Mendax1 <abaris004@gmail.com>
2023-01-30 00:33:50 +0100

    nothing new

--- HMI/src/Main.java
+++ HMI/src/Main.java
@@ -1,6 +1,7 @@
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         //ClickPage clickPage = new ClickPage();
         StartPage startPage = new StartPage();
@@ -14,8 +15,10 @@ public class Main {
         players.add(new Player(300, "Maximilian"));
         players.add(new Player(400, "Elias"));
         players.add(new Player(500, "Johannes"));
+        players.add(new Player(-50, "Lukas"));
 
         fileHandler.write(players, 600,"Test","HMI/src/scores.txt");
+        fileHandler.write(players, 700,"Test2","HMI/src/scores.txt");
         ArrayList<Player> stuff = fileHandler.readFile("HMI/src/scores.txt");
         for (Player player : stuff) {
             System.out.println(player.getScore() + " " + player.getName());

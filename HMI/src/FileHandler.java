ee3c4f486884c86edd046e2e3dcc1b9c12952329
Mendax1 <abaris004@gmail.com>
2023-01-30 00:32:55 +0100

    sort rausgenommen(ist über comparable interface von player class integriert Scanner statt bufferedreader

--- HMI/src/FileHandler.java
+++ HMI/src/FileHandler.java
@@ -1,60 +1,39 @@
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
 public class FileHandler {
-    //Einlesen der scores von text file in Arraylist und zurückgeben
     public ArrayList<Player> readFile(String filePath) {
         ArrayList<Player> players = new ArrayList<>();
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(filePath));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] parts = line.split("\\) ");//regex um score in klammern und name zu trennen
+        try (Scanner scanner = new Scanner(new File(filePath))) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] parts = line.split("\\) ");
                 int score = Integer.parseInt(parts[0].substring(1));
                 String name = parts[1];
-                Player player = new Player(score, name);
-                players.add(player);
+                players.add(new Player(score, name));
             }
-            reader.close();
         } catch (FileNotFoundException e) {
             System.out.println("Could not find the file '" + filePath + "'.");
-        } catch (IOException e) {
-            System.out.println("An error occurred while reading the file '" + filePath + "'.");
         }
+        Collections.sort(players);
         return players;
     }
-    //writeproto als prototyp für neue Methode um nicht ganze Liste zu überschreiben
-    //sollte verwendet werden, damit nicht ganze Text datei in arraylist geladen werden muss
-    //nur einzelne Werte sollten überschrieben werden
-    public void writeproto(int score, String name, String filePath) throws IOException {
-        //File path als "src/scores.txt" übergeben
-        //zb Filehandler.write(score, name, "src/scores.txt");
-        BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true));
-        writer.write("(" + score + ")" + " " + name);//score wird in Klammern gespeichert
-        writer.newLine();
-        writer.close();
-    }
-    public void write(ArrayList<Player> players,int score, String name, String filePath) throws IOException {
+
+    public void write(ArrayList<Player> players, int score, String name, String filePath) throws IOException {
         players.add(new Player(score, name));
-        sort(players);
-        BufferedWriter writer = new BufferedWriter(new FileWriter(filePath));
-        for (Player player : players) {
-            writer.write("(" + player.getScore() + ")" + " " + player.getName());
-            writer.newLine();
-        }
-        writer.close();
-    }
-    public void sort(ArrayList<Player> players){
-        //sortiert die ArrayList nach score
-        for (int i = 0; i < players.size(); i++) {
-            for (int j = 0; j < players.size() - 1; j++) {
-                if (players.get(j).getScore() < players.get(j + 1).getScore()) {
-                    Player temp = players.get(j);
-                    players.set(j, players.get(j + 1));
-                    players.set(j + 1, temp);
-                }
+        Collections.sort(players);
+        try (FileWriter writer = new FileWriter(filePath)) {
+            for (Player player : players) {
+                writer.write("(" + player.getScore() + ")" + " " + player.getName());
+                writer.write(System.lineSeparator());
             }
         }
     }
 
 }
+
